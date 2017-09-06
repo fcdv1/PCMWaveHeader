1f02c907312558db7fd93f030d5f1e033d38f266
zfh <245185601@qq.com>
2017-09-06 15:49:34 +0800

    add test file

--- WAVFilePakage/ViewController.m
+++ WAVFilePakage/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "WAVFilePakage.h"
 
 @interface ViewController ()
 
@@ -17,13 +18,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+- (IBAction)testFileButtonPressed:(id)sender {
+    NSString *inputFile = [[NSBundle mainBundle] pathForResource:@"pcmNoHeader" ofType:@"wav"];
+    NSString *outputPath = [NSString stringWithFormat:@"%@/Documents/pcmWithHeader.wav",NSHomeDirectory()];
+    generateFile((char *)inputFile.UTF8String, 0x5622, 2, 16, (char *)outputPath.UTF8String);
+    NSData *data1 = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"originnalWav" ofType:@"wav"]];
+    NSData *data2 = [NSData dataWithContentsOfFile:outputPath];
+    uint8_t a,b;
+    BOOL equal = YES;
+    for (int i = 0; i < data1.length; i++) {
+        a = ((uint8_t *)data1.bytes)[i];
+        b = ((uint8_t *)data2.bytes)[i];
+        if (a != b) {
+            equal = NO;
+            break;
+        }
+    }
+    if (equal) {
+         NSLog(@"相等");
+    } else {
+         NSLog(@"不相等");
+    }
+}
+- (IBAction)testBufferButtonPressed:(id)sender {
+    NSString *inputFile = [[NSBundle mainBundle] pathForResource:@"pcmNoHeader" ofType:@"wav"];
+    NSData *pcmData = [NSData dataWithContentsOfFile:inputFile];
+    
+    unsigned char *waveBuffer = NULL;
+    int waveBufferLength = 0;
+    generateWaveBuffer((unsigned char *)pcmData.bytes,(int)pcmData.length, 0x5622, 2, 16, &waveBuffer, &waveBufferLength);
+    NSData *data1 = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"originnalWav" ofType:@"wav"]];
+    uint8_t a,b;
+    BOOL equal = YES;
+    for (int i = 0; i < data1.length; i++) {
+        a = ((uint8_t *)data1.bytes)[i];
+        b = waveBuffer[i];
+        if (a != b) {
+            equal = NO;
+            break;
+        }
+    }
+    if (equal) {
+        NSLog(@"相等");
+    } else {
+        NSLog(@"不相等");
+    }
+    
+    for (int i = 0; i < 0x2b; i++) {
+        printf("%x ",waveBuffer[i]);
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
